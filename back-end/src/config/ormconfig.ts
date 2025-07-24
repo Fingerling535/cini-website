5a9856f5de56628ba0c87ecd4bfcc0e2a4fe368e
kaan doğan <wowfalcon535@gmail.com>
2025-07-24 22:45:04 +0300

    Update ormconfig.ts

--- back-end/src/config/ormconfig.ts
+++ back-end/src/config/ormconfig.ts
@@ -10,8 +10,8 @@ const ormconfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAMENAME,
-  password: process.env.DB_PASSWORDWORD,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
   // ★ Burada iki seçenek:
