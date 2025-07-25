1740936404a8afcca18d566f9ebaa20a83809d90
Kaan Doğan <wowfalcon535@gmail.com>
2025-07-25 21:07:38 +0300

    Update db.ts

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,23 +1,16 @@
 
-
 // src/config/db.ts
-export const initializeDatabase = async () => {
-  console.log('⚠️ Database bağlantısı devre dışı bırakıldı');
-};
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import ormconfig from '../config/ormconfig';
 
+export const AppDataSource = new DataSource(ormconfig);
 
-// // src/config/db.ts
-// import 'reflect-metadata';
-// import { DataSource } from 'typeorm';
-// import ormconfig from '../config/ormconfig';
-
-// export const AppDataSource = new DataSource(ormconfig);
-
-// export function initializeDatabase() {
-//   return AppDataSource.initialize()
-//     .then(() => console.log('📦 PostgreSQL’e bağlandı'))
-//     .catch(err => {
-//       console.error('🚨 Veritabanı bağlantı hatası:', err);
-//       process.exit(1);
-//     });
-// }
+export function initializeDatabase() {
+  return AppDataSource.initialize()
+    .then(() => console.log('📦 PostgreSQL’e bağlandı'))
+    .catch(err => {
+      console.error('🚨 Veritabanı bağlantı hatası:', err);
+      process.exit(1);
+    });
+}
